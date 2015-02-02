012258b43019e98e617c3208c76e9371f7512e1c
Christophe PALLIER <christophe@pallier.org>
2015-02-02 12:34:11 +0100

    added lexical decision

---
+++ GeneralIntro/auto/generalintro.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "generalintro"
+ (lambda ()
+    (TeX-add-symbols
+     "seti"
+     "conti")
+    (TeX-run-style-hooks
+     "times"
+     "graphicx"
+     "skak"
+     "verbatim"
+     "amsmath"
+     "qtree"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "")))
+
